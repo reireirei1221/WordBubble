17a4d32fac572dc7d4053f4805eb6c29fa873eeb
reireirei1221 <72299684+reireirei1221@users.noreply.github.com>
2023-06-04 19:30:16 +0900

    Update index.blade.php
    Webページの名前を変更しました

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Blog</title>
+        <title>WordBubble</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="/css/style.css" >
